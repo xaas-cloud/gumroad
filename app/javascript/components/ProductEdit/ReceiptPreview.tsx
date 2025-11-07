715170a804c65cc4dc2680b328309619261a1de2
andev18 <andev18fullstack@gmail.com>
2025-11-07 22:18:16 +0700

    fix: polish code

--- app/javascript/components/ProductEdit/ReceiptPreview.tsx
+++ app/javascript/components/ProductEdit/ReceiptPreview.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { request, assertResponseError, ResponseError } from "$app/utils/request";
+import { request } from "$app/utils/request";
 
 import { useProductEditContext } from "$app/components/ProductEdit/state";
 import { useDebouncedCallback } from "$app/components/useDebouncedCallback";
@@ -13,65 +13,35 @@ export const ReceiptPreview = () => {
     product: { custom_receipt_text, custom_view_content_button_text },
   } = useProductEditContext();
   const [receiptHtml, setReceiptHtml] = React.useState<string>("");
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [error, setError] = React.useState<string | null>(null);
 
   const fetchReceiptPreview = React.useCallback(async () => {
     if (!uniquePermalink) return;
 
-    try {
-      setIsLoading(true);
-      setError(null);
-
-      const params = new URLSearchParams();
-      if (custom_receipt_text) {
-        params.append("custom_receipt_text", custom_receipt_text);
-      }
-      if (custom_view_content_button_text) {
-        params.append("custom_view_content_button_text", custom_view_content_button_text);
-      }
-
-      const url = `/internal/products/${uniquePermalink}/receipt_preview${params.toString() ? `?${params.toString()}` : ""}`;
-
-      const response = await request({
-        method: "GET",
-        url,
-        accept: "html",
-      });
-
-      if (!response.ok) throw new ResponseError("Server returned error response");
-
-      const html = await response.text();
-
-      setReceiptHtml(html);
-    } catch (e) {
-      assertResponseError(e);
-      setError(e.message);
-    } finally {
-      setIsLoading(false);
+    const params = new URLSearchParams();
+    if (custom_receipt_text) {
+      params.append("custom_receipt_text", custom_receipt_text);
     }
+    if (custom_view_content_button_text) {
+      params.append("custom_view_content_button_text", custom_view_content_button_text);
+    }
+
+    const url = `/internal/products/${uniquePermalink}/receipt_preview${params.toString() ? `?${params.toString()}` : ""}`;
+
+    const response = await request({
+      method: "GET",
+      url,
+      accept: "html",
+    });
+
+    const html = await response.text();
+
+    setReceiptHtml(html);
   }, [uniquePermalink, custom_receipt_text, custom_view_content_button_text]);
 
   const debouncedFetchReceiptPreview = useDebouncedCallback(() => void fetchReceiptPreview(), 300);
 
   useRunOnce(() => void fetchReceiptPreview());
   useOnChange(debouncedFetchReceiptPreview, [uniquePermalink, custom_receipt_text, custom_view_content_button_text]);
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center p-8">
-        <div className="text-secondary">Loading receipt preview...</div>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex items-center justify-center p-8">
-        <div className="text-red-600">{error}</div>
-      </div>
-    );
-  }
 
   return <div dangerouslySetInnerHTML={{ __html: receiptHtml }} />;
 };
