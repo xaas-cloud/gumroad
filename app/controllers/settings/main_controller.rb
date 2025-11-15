aa9c1e11999373b365c5e217053afa51f7e68fed
Pradumn <pradumn@tealfeed.com>
2025-11-15 23:50:21 +0530

    fix: wait for ajax spec replacement for inertia

--- app/controllers/settings/main_controller.rb
+++ app/controllers/settings/main_controller.rb
@@ -5,6 +5,28 @@ class Settings::MainController < Settings::BaseController
 
   before_action :authorize
 
+  USER_ATTRIBUTE_KEYS = [
+    :email,
+    :enable_payment_email,
+    :enable_payment_push_notification,
+    :enable_recurring_subscription_charge_email,
+    :enable_recurring_subscription_charge_push_notification,
+    :enable_free_downloads_email,
+    :enable_free_downloads_push_notification,
+    :announcement_notification_enabled,
+    :disable_comments_email,
+    :disable_reviews_email,
+    :support_email,
+    :locale,
+    :timezone,
+    :currency_type,
+    :purchasing_power_parity_enabled,
+    :purchasing_power_parity_limit,
+    :purchasing_power_parity_payment_verification_disabled,
+    :show_nsfw_products,
+    :disable_affiliate_requests,
+  ].freeze
+
   def show
     @title = "Settings"
 
@@ -12,28 +34,26 @@ class Settings::MainController < Settings::BaseController
   end
 
   def update
-    current_seller.with_lock { current_seller.update!(user_params) }
+    user_payload = normalized_user_payload
 
-    if params[:user][:email] == current_seller.email
-      current_seller.update!(unconfirmed_email: nil)
+    current_seller.with_lock do
+      previous_email = current_seller.email
+      current_seller.assign_attributes(user_payload.slice(*USER_ATTRIBUTE_KEYS))
+      current_seller.unconfirmed_email = nil if user_payload[:email] == previous_email
+
+      apply_refund_policy!(user_payload[:seller_refund_policy]) if current_seller.account_level_refund_policy_enabled?
+      current_seller.save!
     end
 
-    if current_seller.account_level_refund_policy_enabled?
-      current_seller.refund_policy.update!(
-        max_refund_period_in_days: seller_refund_policy_params[:max_refund_period_in_days],
-        fine_print: seller_refund_policy_params[:fine_print],
-      )
-    end
+    current_seller.update_purchasing_power_parity_excluded_products!(normalized_ppp_ids(user_payload))
+    current_seller.update_product_level_support_emails!(normalized_product_level_support_emails(user_payload))
 
-    current_seller.update_purchasing_power_parity_excluded_products!(params[:user][:purchasing_power_parity_excluded_product_ids])
-    current_seller.update_product_level_support_emails!(params[:user][:product_level_support_emails])
-
-    render json: { success: true }
+    render_main_page
   rescue StandardError => e
     Bugsnag.notify(e)
-    error_message = current_seller.errors.full_messages.to_sentence.presence ||
+    message = current_seller.errors.full_messages.to_sentence.presence ||
       "Something broke. We're looking into what happened. Sorry about this!"
-    render json: { success: false, error_message: }
+    handle_inertia_error(message)
   end
 
   def resend_confirmation_email
@@ -45,38 +65,43 @@ class Settings::MainController < Settings::BaseController
   end
 
   private
-    def user_params
-      permitted_params = [
-        :email,
-        :enable_payment_email,
-        :enable_payment_push_notification,
-        :enable_recurring_subscription_charge_email,
-        :enable_recurring_subscription_charge_push_notification,
-        :enable_free_downloads_email,
-        :enable_free_downloads_push_notification,
-        :announcement_notification_enabled,
-        :disable_comments_email,
-        :disable_reviews_email,
-        :support_email,
-        :locale,
-        :timezone,
-        :currency_type,
-        :purchasing_power_parity_enabled,
-        :purchasing_power_parity_limit,
-        :purchasing_power_parity_payment_verification_disabled,
-        :show_nsfw_products,
-        :disable_affiliate_requests,
-      ]
-
-      params.require(:user).permit(permitted_params)
+    def normalized_user_payload
+      @normalized_user_payload ||= params.require(:user).to_unsafe_h.deep_symbolize_keys
     end
 
-    def seller_refund_policy_params
-      params[:user][:seller_refund_policy]&.permit(:max_refund_period_in_days, :fine_print)
+    def apply_refund_policy!(refund_policy_params)
+      return if refund_policy_params.blank?
+
+      current_seller.refund_policy.update!(
+        max_refund_period_in_days: refund_policy_params[:max_refund_period_in_days],
+        fine_print: refund_policy_params[:fine_print],
+      )
     end
 
-    def product_level_support_emails_params
-      params[:user][:product_level_support_emails]&.permit(:email, { product_ids: [] })
+    def normalized_ppp_ids(user_payload)
+      Array.wrap(user_payload[:purchasing_power_parity_excluded_product_ids]).filter_map(&:presence)
+    end
+
+    def normalized_product_level_support_emails(user_payload)
+      Array.wrap(user_payload[:product_level_support_emails]).filter_map do |entry|
+        next if entry.blank?
+
+        email = entry[:email].to_s.strip
+        product_ids = Array.wrap(entry[:product_ids]).filter_map(&:presence)
+        next if email.blank? || product_ids.blank?
+
+        { email:, product_ids: }
+      end
+    end
+
+    def handle_inertia_error(message)
+      redirect_to settings_main_path,
+                  inertia: { errors: { error_message: message } },
+                  status: :see_other
+    end
+
+    def render_main_page(status: :ok)
+      render inertia: "Settings/Main", props: settings_presenter.main_props, status: status
     end
 
     def fetch_discover_sales(seller)
--- spec/requests/settings/main_spec.rb
+++ spec/requests/settings/main_spec.rb
@@ -30,7 +30,6 @@ describe("Main Settings Scenario", type: :system, js: true) do
         fill_in("Email", with: new_email)
       end
       click_on("Update settings")
-      wait_for_ajax
 
       expect(page).to have_alert(text: "Your account has been updated!")
       expect(user.reload.unconfirmed_email).to eq new_email
@@ -49,7 +48,7 @@ describe("Main Settings Scenario", type: :system, js: true) do
 
       within_section "User details", section_element: :section do
         validation_message = find_field("Email").native.attribute("validationMessage")
-        expect(validation_message).to eq "Please fill out this field."
+        expect(validation_message).to eq "Please fill in this field."
       end
     end
   end
@@ -80,7 +79,6 @@ describe("Main Settings Scenario", type: :system, js: true) do
         expect(page).to have_text("This email address has not been confirmed yet. Resend confirmation?")
         click_on "Resend confirmation?"
 
-        wait_for_ajax
         expect(page).to have_alert(text: "Confirmation email resent!")
         expect(page).to_not have_link("Resend confirmation?")
       end
@@ -152,7 +150,6 @@ describe("Main Settings Scenario", type: :system, js: true) do
         end
 
         click_on "Update settings"
-        wait_for_ajax
 
         expect(page).to have_alert(text: "Your account has been updated!")
         user.reload
@@ -173,7 +170,7 @@ describe("Main Settings Scenario", type: :system, js: true) do
         end
 
         click_on "Update settings"
-        wait_for_ajax
+        expect(page).to have_alert(text: "Your account has been updated!")
         user.reload
 
         expect(user.purchasing_power_parity_excluded_product_external_ids).to eq([@product_1.external_id, @product_2.external_id])
@@ -195,7 +192,7 @@ describe("Main Settings Scenario", type: :system, js: true) do
         end
 
         click_on "Update settings"
-        wait_for_ajax
+        expect(page).to have_alert(text: "Your account has been updated!")
         user.reload
 
         expect(user.purchasing_power_parity_excluded_product_external_ids).to eq([])
@@ -231,9 +228,11 @@ describe("Main Settings Scenario", type: :system, js: true) do
     expect(page).to have_alert(text: "Your account has been updated!")
     expect(user.reload.show_nsfw_products).to eq(true)
 
+    visit settings_main_path
+    expect(page).to have_checked_field("Show adult content in recommendations and search results")
+
     uncheck "Show adult content in recommendations and search results"
     click_on "Update settings"
-    wait_for_ajax
 
     expect(page).to have_alert(text: "Your account has been updated!")
     expect(user.reload.show_nsfw_products).to eq(false)
@@ -255,7 +254,6 @@ describe("Main Settings Scenario", type: :system, js: true) do
         fill_in "Fine print", with: "This is a sample fine print"
 
         click_on "Update settings"
-        wait_for_ajax
 
         expect(page).to have_alert(text: "Your account has been updated!")
 
