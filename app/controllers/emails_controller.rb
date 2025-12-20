fbdf10fbbbf922ee52f60b22259d9a9169b6e9c6
stefan binoj <stefan.binoj.007@gmail.com>
2025-12-20 12:48:50 +0530

    update controler to handle other routes

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,20 +1,19 @@
 # frozen_string_literal: true
 
 class EmailsController < Sellers::BaseController
-  # TODO: Remove set_body_id_as_app once all email pages are migrated to Inertia
-  before_action :set_body_id_as_app, only: %i[index]
-  before_action :set_inertia_layout, only: %i[published scheduled]
+  layout "inertia", only: [:published, :scheduled]
+
+  before_action :set_body_id_as_app, only: [:index]
 
   def index
     authorize Installment
 
-    create_user_event("emails_view")
-
-    # TODO: Remove this redirect logic once all email pages are migrated to Inertia
-    # For now, /emails/drafts, /emails/new, /emails/:id/edit are still handled by react-router
     if request.path == emails_path
-      default_tab = Installment.alive.not_workflow_installment.scheduled.where(seller: current_seller).exists? ? "scheduled" : "published"
-      redirect_to "#{emails_path}/#{default_tab}", status: :moved_permanently
+      if current_seller.installments.alive.not_workflow_installment.scheduled.exists?
+        return redirect_to scheduled_emails_path, status: :moved_permanently
+      else
+        return redirect_to published_emails_path, status: :moved_permanently
+      end
     end
   end
 
@@ -22,6 +21,7 @@ class EmailsController < Sellers::BaseController
     authorize Installment, :index?
     create_user_event("emails_view")
 
+    @title = "Published Emails"
     presenter = PaginatedInstallmentsPresenter.new(seller: current_seller, type: Installment::PUBLISHED, page: 1)
     render inertia: "Emails/Published", props: presenter.props
   end
@@ -30,16 +30,9 @@ class EmailsController < Sellers::BaseController
     authorize Installment, :index?
     create_user_event("emails_view")
 
+    @title = "Scheduled Emails"
     presenter = PaginatedInstallmentsPresenter.new(seller: current_seller, type: Installment::SCHEDULED, page: 1)
     render inertia: "Emails/Scheduled", props: presenter.props
   end
 
-  private
-    def set_title
-      @title = "Emails"
-    end
 
-    def set_inertia_layout
-      self.class.layout "inertia"
-    end
-end
